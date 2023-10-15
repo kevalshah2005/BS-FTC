96e049776012925fbf74b47a73c11cec157ac9d5
Keval Shah <kevals2005@gmail.com>
2023-10-14 19:05:01 -0500

    Added new response

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -13,6 +13,8 @@ class MessageParser {
             this.actionProvider.message("What, do you not have anything to say? Guess you're in awe of the great BS FTC's presense.");
         } else if (lowercase.includes("hello")) {
             this.actionProvider.message("You are not worthy of speaking like that to the great BS FTC.");
+        } else if (lowercase.includes("bye")) {
+            this.actionProvider.message("Goodbye. Leave at once.");
         } else {
             this.actionProvider.sendLink();
         }
