a94df79e564531759a7ae0281dc140e256b9affa
Keval Shah <kevals2005@gmail.com>
2023-10-15 17:15:50 -0500

    Fixed typos and added more responses

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -8,15 +8,15 @@ class MessageParser {
         this.map.set("foster", "Don't you DARE utter that name in my presense!");
         this.map.set("ftcgpt", "Don't you DARE utter that name in my presense!");
         this.map.set("ftc gpt", "Don't you DARE utter that name in my presense!");
-        this.map.set("game manual", "I possess all of the knowledge of all of the game manuals, but you may not have any of it. Mwahahahahaha!");
+        this.map.set("manual", "I possess all of the knowledge of all of the game manuals, but you may not have any of it. Mwahahahahaha!");
         this.map.set("rule", "Rules? Do you seriously follow those? Just get on the field and yeet your opponent's robot into next week!");
         this.map.set("bye", "Goodbye. Leave at once.");
         this.map.set("inspire", "The judges are so inspired by me that I, the great BS FTC, automatically win every inspire award at every competition.");
-        this.map.set("win", "Is that even a question? I, the great BS FTC, always wins.");
-        this.map.set("lose", "I don't know who will lose, but I, the great BS FTC, am assuredly not the one who would lose.");
+        this.map.set("win", "Is that even a question? I, the great BS FTC, always win.");
+        this.map.set("lose", "I don't know who will lose, but I, the great BS FTC, will assuredly not lose.");
         this.map.set("captain", "It doesn't matter who the captain is. They will never beat the great BS FTC.");
         this.map.set("outreach", "Who needs outreach when you can just win every competition like I, the great BS FTC, do?");
-        this.map.set("portfolio", "Who needs a portfolio");
+        this.map.set("portfolio", "Who needs a portfolio?");
         this.map.set("gracious professionalism", "Gracious professionalism is a myth. If you win hard enough, who cares?");
         this.map.set("control hub", "I, the great BS FTC, possess the entire supply of control hubs. You may not have one. Mwahahahahaha!");
         this.map.set("autonomous", "Just control your robot using a controller. Make sure they don't catch you by using your 1,000,000 lumen LED panel to blind them!");
@@ -27,7 +27,10 @@ class MessageParser {
         this.map.set("april tag", "Just use a color sensor to detect AprilTags!");
         this.map.set("axon", "If you repeatedly ping Veer in the FTC server, he'll eventually respond to your query!");
         this.map.set("swerve", "Swerve is the only viable drivetrain in FTC. Make sure to use it!");
-        
+        this.map.set("android studio", "Just use Google Docs instead. It's a much better IDE. Why do you think all of the software engineering job interviews use it?");
+        this.map.set("ide", "Use Google Docs. Why do you think all of the software engineering job interviews use it?");
+        this.map.set("allow", "You're allowed to do whatever you want as long as you bribe the refs hard enough!");
+        this.map.set("meta", "The meta is whatever robot you build. Go wild!");
     }
 
     parse(message) {
